eac9278cf8f63f163c9afd4bca7e64ff07ccce77
nani454 <nani454@gmail.com>
2021-08-16 00:29:18 +0530

    Two threads printing even and odd numbers synchronously.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Project to work on features and challanges
 
 Explore lamdas of java 8 under ExploringLamda.java file
+
+Printing even and odd numbers using two different threads - EvenOddThreads.java
+
---
+++ src/com/nani454/features/EvenOddThreads.java
@@ -0,0 +1,66 @@
+package com.nani454.features;
+
+public class EvenOddThreads {
+    public static void main(String[] args) {
+        int n = 15;
+        Printer printer = new Printer(n);
+        Thread evenThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                printer.printEvenNumbers(n);
+            }
+        });
+        Thread oddThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                printer.printOddNumbers(n);
+            }
+        });
+
+        evenThread.start();
+
+        oddThread.start();
+    }
+}
+class Printer {
+    final int n;
+    int counter = 1;
+
+    public Printer(int n) {
+        this.n = n;
+    }
+
+    public void printEvenNumbers(int n) {
+        synchronized(this) {
+            while (counter < n) {
+                while (counter % 2 != 0) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(counter + " ");
+                counter++;
+                notify();
+            }
+        }
+    }
+
+    public void printOddNumbers(int n) {
+        synchronized (this) {
+            while (counter < n) {
+                while (counter % 2 == 0) {
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.print(counter + " ");
+                counter++;
+                notify();
+            }
+        }
+    }
+}
